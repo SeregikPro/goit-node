f363613f0539f5c5f5d452498a6aac4698c9184b
Sergii Proiava <seregikpro@gmail.com>
2022-10-08 20:28:32 +0300

    add connection to MongoDB, function updateStatusContact

---
+++ controllers/contacts/index.js
@@ -0,0 +1,15 @@
+const listContacts = require("./listContacts");
+const getContactById = require("./getContactById");
+const addContact = require("./addContact");
+const updateContactById = require("./updateContactById");
+const updateStatusContact = require("./updateStatusContact");
+const removeContact = require("./removeContact");
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContactById,
+  updateStatusContact,
+  removeContact,
+};
